629789992a13d204563a6cc103273814bac05b12
rubbyzhang <rubbyzhang@tencent.com>
2021-02-20 20:11:26 +0800

    增加点位采集功能 V1

--- Source/UnrealCV/Private/Controller/WorldController.cpp
+++ Source/UnrealCV/Private/Controller/WorldController.cpp
@@ -34,7 +34,7 @@ void AUnrealcvWorldController::AttachPawnSensor()
 	UPawnCamSensor* PawnCamSensor = NewObject<UPawnCamSensor>(Pawn, TEXT("PawnSensor")); // Make Pawn as the owner of the component
 	// UFusionCamSensor* FusionCamSensor = ConstructObject<UFusionCamSensor>(UFusionCamSensor::StaticClass(), Pawn);
 
-	UWorld *PawnWorld = Pawn->GetWorld(), *GameWorld = FUnrealcvServer::Get().GetWorld();
+	UWorld* PawnWorld = Pawn->GetWorld(), * GameWorld = FUnrealcvServer::Get().GetWorld();
 	// check(Pawn->GetWorld() == FUnrealcvServer::GetWorld());
 	check(PawnWorld == GameWorld);
 	PawnCamSensor->AttachToComponent(Pawn->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
@@ -80,8 +80,6 @@ void AUnrealcvWorldController::BeginPlay()
 	Super::BeginPlay();
 }
 
-
-
 void AUnrealcvWorldController::OpenLevel(FName LevelName)
 {
 	UWorld* World = GetWorld();
@@ -97,3 +95,137 @@ void AUnrealcvWorldController::Tick(float DeltaTime)
 {
 
 }
+
+
+//float	 FovX;
+//float	 FovY;
+//uint32   PitchNum;
+//uint32   YawNum;
+//uint32   ImageWidth;
+//uint32   ImageHeight;
+//FString  FolderPath;
+
+
+bool AUnrealcvWorldController::Capture(const FCaptureParamer& InCaptureParamter, const TArray<FVector>& InPoints)
+{
+	UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::Capture Start"));
+
+	if (InPoints.Num() == 0)
+	{
+		UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::Capture : Capture Point List is empty"));
+		return false;
+	}
+
+	APawn* Pawn = FUnrealcvServer::Get().GetPawn();
+	if (!IsValid(Pawn))
+	{
+		UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::Capture : The Pawn of the scene is invalid."));
+		return false;
+	}
+
+	CaptureParamter = InCaptureParamter;
+
+	// Set Parameters(Use FCaptureParamer)
+	// Fov
+	//ImageWidth
+	//ImageHeight
+	//OuptputPath
+	//
+
+	GenerateOrientations(InCaptureParamter, InPoints, CaptureOrientations);
+
+	OldCameraOrientation.Location = Pawn->GetActorLocation();
+	OldCameraOrientation.Rotator = Pawn->GetActorRotation();
+
+	CurCaptureIndex = 0;
+	SetTransfromDelay();
+
+	UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::Capture End"));
+
+	return false;
+}
+
+void AUnrealcvWorldController::SetTransfromDelay()
+{
+	UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::SetTransfromDelay, Index: %d"), CurCaptureIndex);
+
+	if (CurCaptureIndex < CaptureOrientations.Num())
+	{
+		SetCameraOrientation(CaptureOrientations[CurCaptureIndex]);
+		GetWorldTimerManager().SetTimerForNextTick(this, &AUnrealcvWorldController::ScreenShotDelay);
+	}
+	else
+	{
+		SetCameraOrientation(OldCameraOrientation);
+
+		UE_LOG(LogUnrealCV, Error, TEXT("AUnrealcvWorldController::SetTransfromDelay End"));
+	}
+}
+
+void AUnrealcvWorldController::ScreenShotDelay()
+{
+	FString FilePath = CaptureParamter.TargetDirectory + FString::FromInt(CaptureOrientations[CurCaptureIndex].PointId) + "-" + FString::FromInt(CaptureOrientations[CurCaptureIndex].InterId) + ".png";
+	ScreenShot(FilePath);
+	CurCaptureIndex++;
+	GetWorldTimerManager().SetTimerForNextTick(this, &AUnrealcvWorldController::SetTransfromDelay);
+}
+
+void AUnrealcvWorldController::GenerateOrientations(const FCaptureParamer& InCaptureParamters, const TArray<FVector>& InPoints, TArray<FCaptureOrientation>& Orientations)
+{
+	Orientations.Empty();
+
+	float YawAngleGap = 360.0f / (float)InCaptureParamters.YawNum;
+	float PitchAngleGap = 360.0f / (float)InCaptureParamters.PitchNum;
+
+	for (int32 PointIndex = 0; PointIndex < InPoints.Num(); ++PointIndex)
+	{
+		for (int32 PitchIndex = 0; PitchIndex < InCaptureParamters.PitchNum; ++PitchIndex)
+		{
+			float CurPitctAngle = PitchIndex * PitchAngleGap;
+
+			for (int32 YawIndex = 0; YawIndex < InCaptureParamters.YawNum; ++YawIndex)
+			{
+				float CurYawAngle = YawIndex * YawAngleGap;
+
+				//TODO Roll Angle = 0
+				FRotator Rotator = FRotator(CurPitctAngle, CurYawAngle, 0);
+
+				FCaptureOrientation Orientation;
+				Orientation.Rotator = Rotator;
+				Orientation.Location = InPoints[PointIndex];
+				Orientation.PointId = PointIndex;
+				Orientation.InterId = PitchIndex * InCaptureParamters.YawNum + YawIndex;
+
+				UE_LOG(LogUnrealCV, Warning, TEXT("AUnrealcvWorldController::Capture : Location - %s , CurPitctAngle - %f , CurYawAngle - %f "), *InPoints[PointIndex].ToCompactString(), CurPitctAngle, CurYawAngle);
+
+				Orientations.Add(Orientation);
+			}
+		}
+	}
+}
+
+bool  AUnrealcvWorldController::SetCameraOrientation(const FCaptureOrientation& Orientation)
+{
+	// Set Transform
+	APawn* Pawn = FUnrealcvServer::Get().GetPawn();
+	if (!IsValid(Pawn))
+	{
+		return false;
+	}
+
+	UE_LOG(LogUnrealCV, Warning, TEXT("AUnrealcvWorldController::CameraShot : Location - %s , Rotator - %s"), *Orientation.Location.ToCompactString(), *Orientation.Rotator.ToCompactString());
+	Pawn->SetActorLocation(Orientation.Location, false, NULL, ETeleportType::TeleportPhysics);
+	Pawn->SetActorRotation(Orientation.Rotator, ETeleportType::TeleportPhysics);
+
+	return true;
+}
+
+//TODO How to use HighResShot
+void AUnrealcvWorldController::ScreenShot(const FString& SaveFilePath)
+{
+	FString Cmd = "vget /screenshot " + SaveFilePath;
+	UE_LOG(LogUnrealCV, Warning, TEXT("AUnrealcvWorldController::ScreenShot:  ScreenShot Command is %s"), *Cmd);
+	FExecStatus ExecStatus1 = FUnrealcvServer::Get().CommandDispatcher->Exec(Cmd);
+	UE_LOG(LogUnrealCV, Warning, TEXT("%s"), *ExecStatus1.GetMessage());
+}
+
--- Source/UnrealCV/Public/WorldController.h
+++ Source/UnrealCV/Public/WorldController.h
@@ -8,6 +8,46 @@
 #include "PlayerViewMode.h"
 #include "WorldController.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct FCaptureParamer
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	float	 FovX;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	float	 FovY;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	int32   YawNum;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	int32   PitchNum;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	int32   ImageWidth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	int32   ImageHeight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CaptureParamers")
+	FString  TargetDirectory;
+
+	FCaptureParamer()
+		: FovX(60.0f)
+		, FovY(60.0f)
+		, PitchNum(5)
+		, YawNum(12)
+		, ImageWidth(1920)
+		, ImageHeight(1080)
+		, TargetDirectory("/Output")
+	{
+
+	}
+};
+
 UCLASS()
 class AUnrealcvWorldController : public AActor
 {
@@ -17,7 +57,7 @@ public:
 	FObjectAnnotator ObjectAnnotator;
 
 	UPROPERTY()
-	UPlayerViewMode* PlayerViewMode;
+		UPlayerViewMode* PlayerViewMode;
 
 	AUnrealcvWorldController(const FObjectInitializer& ObjectInitializer);
 
@@ -33,4 +73,31 @@ public:
 	void AttachPawnSensor();
 
 	void Tick(float DeltaTime);
+
+	//----------------------------------------------------------------------------
+	UFUNCTION(BlueprintCallable, Category = "UnrealcvWorldController")
+	bool Capture(const FCaptureParamer& InCaptureParamter, const TArray<FVector>& InPoints);
+
+private:
+
+	struct FCaptureOrientation
+	{
+		FVector  Location;
+		FRotator Rotator;
+		int32    PointId;
+		int32    InterId;
+	};
+
+	FCaptureParamer CaptureParamter;
+
+	FCaptureOrientation  OldCameraOrientation;
+
+	TArray<FCaptureOrientation> CaptureOrientations;
+	int32 CurCaptureIndex; 
+	void ScreenShotDelay();
+	void SetTransfromDelay();
+
+	void GenerateOrientations(const FCaptureParamer& InCaptureParamters, const TArray<FVector>& InPoints , TArray<FCaptureOrientation>& Orientations);
+	bool SetCameraOrientation(const FCaptureOrientation& Orientation);
+	void ScreenShot(const FString& SaveFilePath);
 };
